4c57158a9f8791c9318c6cb8cf37180a042c296b
Simon Beckett <simon@redballoon.com.au>
2018-04-10 19:11:34 +1000

    Replaced Tornado application with aiohttp/asyncio

--- hammerAI/hammerAI.py
+++ hammerAI/hammerAI.py
@@ -1,7 +1,9 @@
-import tornado.httpserver
-import tornado.websocket
-import tornado.ioloop
-import tornado.web
+import asyncio
+import aiohttp.web
+from aiohttp import web
+
+import logging
+import os
 import json
 import socket
 import re
@@ -12,8 +14,12 @@ from uuid import uuid4
 from jose import jwt, exceptions as jose_exceptions
 from const import SERVERS
 
+HOST = os.getenv('HOST', '0.0.0.0')
+PORT = int(os.getenv('PORT', 8080))
+
 wsclients = {}
 msgqueue = {}
+bearerreg = re.compile('Bearer\s(.*)')
 
 info = SERVERS['production']
 cognito_client_id = info['cognito_client_id']
@@ -21,22 +27,10 @@ user_pool_id = info['user_pool_id']
 region = info['region']
 
 pool_url = 'https://cognito-idp.us-east-1.amazonaws.com/{}/.well-known/jwks.json'.format(user_pool_id)
-bearerreg = re.compile('Bearer\s(.*)')
-
-def get_cognito_user(accesstoken):
-    client = boto3.client('cognito-idp', region_name=region)
-    print('AT: {}'.format(accesstoken))
-    resp = client.get_user(
-        AccessToken = accesstoken
-    )
-    print('IDP: {}'.format(resp))
-
-    return resp
 
 def aws_key_dict(aws_region, aws_user_pool):
     aws_data = requests.get(pool_url)
     aws_jwt = json.loads(aws_data.text)
-    # We want a dictionary keyed by the kid, not a list.
     result = {}
     for item in aws_jwt['keys']:
         result[item['kid']] = item
@@ -44,12 +38,9 @@ def aws_key_dict(aws_region, aws_user_pool):
     return result
 
 def get_client_sub_from_access_token(aws_region, aws_user_pool, token):
-    """ Pulls the client ID out of an Access Token
-    """
     claims = get_claims(aws_region, aws_user_pool, token)
     if claims.get('token_use') != 'access':
         raise ValueError('Not an access token')
-
     return claims.get('sub')
 
 def get_user_sub_from_id_token(token):
@@ -57,14 +48,11 @@ def get_user_sub_from_id_token(token):
     return payload.get('sub')
 
 def get_claims(aws_region, aws_user_pool, token, audience=None):
-    # header, _, _ = get_token_segments(token)
     header = jwt.get_unverified_header(token)
     kid = header['kid']
 
     verify_url = pool_url
-
     keys = aws_key_dict(aws_region, aws_user_pool)
-
     key = keys.get(kid)
 
     kargs = {"issuer": verify_url}
@@ -79,7 +67,6 @@ def get_claims(aws_region, aws_user_pool, token, audience=None):
             'verify_exp': False,
         }
     )
-
     return claims
 
 def get_intended_user(authheader):
@@ -97,64 +84,54 @@ def handlepl(shhandler, shpayload):
     }
     return hasspayload
 
-def fetchpayload(msgid):
-    payload = None
-    print('Fetching: {}'.format(msgid))
-    while payload is None:
-        print('Polling Queue')
-        #print('Queue Contains on Fetch: {}'.format(msgqueue))
-        payload = msgqueue.get(msgid)
-    print('Fetched: {}'.format(payload))
-    return payload    
+async def fetchpayload(msgid):
+    fetchedpayload = None
+    logging.info('Waiting for: {}'.format(msgid))
+    while fetchedpayload is None:
+        await asyncio.sleep(0.2)
+        fetchedpayload = msgqueue.pop(msgid)
+    logging.info('Fetched: {}'.format(fetchedpayload['msgid']))
+    payload = fetchedpayload['payload']
+    return payload
 
-def storemsg(message):
-    try:
-        print('Storing MSGID: {}'.format(message['msgid']))
-        msgqueue[message['msgid']] = message
-        print('Queue Contains: {}'.format(msgqueue))
-    except ValueError:
-        disconnect_warn = 'Received invalid JSON.'
+async def ActionsHandler(request):
+    authtokenmatch = bearerreg.match(request.headers['AUTHORIZATION'])
+    gaAuthToken = authtokenmatch.group(1)
+    tgtclient = get_client_sub_from_access_token(region, user_pool_id, gaAuthToken)
+    gapayload = await request.json()
+    payload = handlepl('google_actions', gapayload)
+    msgid = payload['msgid']
+    await wsclients[tgtclient].send_json(payload)
+    response = await fetchpayload(msgid)
+    return web.Response(text=json.dumps(response), status=200)
 
-class ActionsHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def post(self):
-        authtokenmatch = bearerreg.match(self.request.headers['AUTHORIZATION'])
-        tokes = authtokenmatch.group(1)
-        tgtclient = get_client_sub_from_access_token(region, user_pool_id, tokes)
-        gapayload = json.loads(self.request.body)
-        payload = handlepl('google_actions', gapayload)
-        wsclients[tgtclient].write_message(payload)
+async def WSHandler(request):
+    logging.info('Websocket connection starting')
+    ws = aiohttp.web.WebSocketResponse()
+    await ws.prepare(request)
+    logging.info('Websocket connection ready')
+    wsclients[get_intended_user(request.headers['AUTHORIZATION'])] = ws
+    async for msg in ws:
+        if msg.type != aiohttp.WSMsgType.TEXT:
+            disconnect_warn = 'Received non-Text message: {}'.format(msg.type)
+            break
+        msg = msg.json()
+        async def storemsg(message):
+            logging.info('Storing MSGID: {}'.format(message['msgid']))
+            msgqueue[message['msgid']] = message
+        await storemsg(msg)
 
-        
-        response = yield fetchpayload(payload['msgid'])
-        #print('Response: {}'.format(response))
-
-class WSHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print('Connection Opened')
-        wsclients[get_intended_user(self.request.headers['AUTHORIZATION'])] = self
-
-    def on_message(self, message):
-        print ('message recieved')
-        message = json.loads(message)
-        storemsg(message)
-
-    def on_close(self):
-        print('connection closed')
-
-    def check_origin(self, origin):
-        return True
-
-application = tornado.web.Application([
-    (r'/ws', WSHandler),
-    (r'/gactions', ActionsHandler),
-])
-
-if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8080)
-    myIP = socket.gethostbyname(socket.gethostname())
-    print('*** Websocket Server Started at {} ***'.format(myIP))
-    tornado.ioloop.IOLoop.instance().start()
+    print('Websocket connection closed')
+    return ws
 
 
+def main():
+    loop = asyncio.get_event_loop()
+    app = aiohttp.web.Application(loop=loop)
+    app.router.add_route('*', '/gactions', ActionsHandler)
+    app.router.add_route('GET', '/ws', WSHandler)
+    aiohttp.web.run_app(app, host=HOST, port=PORT)
+
+
+if __name__ == '__main__':
+    main()
